bf080b4e2252ef31867bc4f473c3914feb8cd67c
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-01-07 11:28:53 -0500

    New events that handle the mouse entering on the component and leaving the component.

---
+++ src/main/java/io/github/minecraftgui/models/listeners/OnMouseEnterListener.java
@@ -0,0 +1,12 @@
+package io.github.minecraftgui.models.listeners;
+
+import io.github.minecraftgui.models.components.Component;
+
+/**
+ * Created by Samuel on 2015-11-21.
+ */
+public interface OnMouseEnterListener {
+
+    void onMouseEnter(Component component);
+
+}
---
+++ src/main/java/io/github/minecraftgui/models/listeners/OnMouseLeaveListener.java
@@ -0,0 +1,12 @@
+package io.github.minecraftgui.models.listeners;
+
+import io.github.minecraftgui.models.components.Component;
+
+/**
+ * Created by Samuel on 2016-01-07.
+ */
+public interface OnMouseLeaveListener {
+
+    void onMouseLeave(Component component);
+
+}
